0434724c3642757911eaae8a0abe5fde6b6da3a3
Eliezer Pedro <epvj@cin.ufpe.br>
2019-06-01 21:57:38 -0300

    Ajustes na tela de Login

--- Alohomora/app/src/main/java/com/example/alohomora/MainActivity.kt
+++ Alohomora/app/src/main/java/com/example/alohomora/MainActivity.kt
@@ -14,7 +14,7 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateint var providers : List<AuthUI.IdpConfig>
+    lateinit var providers: List<AuthUI.IdpConfig>
 
     val MY_REQUEST_CODE: Int = 7117 // Any number you want
 
@@ -25,9 +25,9 @@ class MainActivity : AppCompatActivity() {
         //init
         providers = Arrays.asList<AuthUI.IdpConfig>(
             AuthUI.IdpConfig.EmailBuilder().build(),
-            AuthUI.IdpConfig.FacebookBuilder().build(),
-            AuthUI.IdpConfig.GoogleBuilder().build(),
-            AuthUI.IdpConfig.Phone().build(),
+            //AuthUI.IdpConfig.FacebookBuilder().build(),
+            //AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.PhoneBuilder().build()
             )
         ShowSignInOptions()
 
@@ -38,52 +38,38 @@ class MainActivity : AppCompatActivity() {
                 .addOnCompleteListener {
                     btn_sign_out.isEnabled=false
                     ShowSignInOptions()
-                    
-
                 }
                 .addOnFailureListener{
                     e-> Toast.makeText( this@MainActivity,e.message,Toast.LENGTH_SHORT).show()
-
                 }
-
         }
-
     }
-
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
         if(requestCode == MY_REQUEST_CODE)
         {
             val response = IdpResponse.fromResultIntent(data)
             if(resultCode == Activity.RESULT_OK)
             {
                 val user = FirebaseAuth.getInstance().currentUser // get current user
-                Toast.makeText(this,""+user.email,Toast.LENGTH_SHORT).show()
-
+                Toast.makeText(this,""+user!!.email,Toast.LENGTH_SHORT).show()
                 btn_sign_out.isEnabled = true
-
             }
             else
             {
                 Toast.makeText(this,""+response!!.error!!.message,Toast.LENGTH_SHORT).show()
-
-
-
             }
-
-
         }
     }
 
 
 
     private fun ShowSignInOptions(){
-        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder())
-            .setAvailableproviders(providers)
+
+        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
+            .setAvailableProviders(providers)
             .setTheme(R.style.MyTheme)
-            .build() ,MY_REQUEST_CODE)
-        )
+            .build(),MY_REQUEST_CODE)
+
     }
 }
